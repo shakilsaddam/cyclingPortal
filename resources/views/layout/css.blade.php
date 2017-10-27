69e91021e05aa76fd53fd3f1be90e5140756a73a
shakilsaddam <saddam.shakil@gmail.com>
2017-10-27 17:25:50 +0600

    trying to create the userhome page.

---
+++ resources/views/layout/css.blade.php
@@ -0,0 +1,27 @@
+<link rel="icon" href="../del-code.com/images/icon/favicon.png" type="image/png">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<meta name="author" content="del-code.com" />
+
+<link href='https://fonts.googleapis.com/css?family=Work+Sans:400,300,600,400italic,700' rel='stylesheet' type='text/css'>
+
+<!-- Animate.css -->
+<link rel="stylesheet" href="css/animate.css">
+<!-- Icomoon Icon Fonts-->
+<link rel="stylesheet" href="css/icomoon.css">
+<!-- Bootstrap  -->
+<link rel="stylesheet" href="css/bootstrap.css">
+
+<!-- Magnific Popup -->
+<link rel="stylesheet" href="css/magnific-popup.css">
+
+<!-- Theme style  -->
+<link rel="stylesheet" href="css/style.css">
+<link rel="stylesheet" type="text/css" href="css/media-query.css">
+
+<!-- Modernizr JS -->
+<script src="js/modernizr-2.6.2.min.js"></script>
+<!-- FOR IE9 below -->
+<!--[if lt IE 9]>
+<script src="js/respond.min.js"></script>
+<![endif]-->
+@yield('content')
---
+++ resources/views/layout/test.blade.php
@@ -0,0 +1,17 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <title>Document</title>
+    <link rel="stylesheet" href="css/bootstrap.css">
+</head>
+<body>
+<div class="container" style="background-color: #0C9CEE">
+    Test Div 1
+</div>
+
+<div class="container" style="background-color: #2ab27b" >
+    Test Div 2
+</div>
+
+</body>
+</html>
