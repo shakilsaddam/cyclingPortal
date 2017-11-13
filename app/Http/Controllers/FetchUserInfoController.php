4779cccf402fc4091ec56432126b0889f6189e35
shakilsaddam <saddam.shakil@gmail.com>
2017-11-13 14:42:51 +0600

    Logout Button added every page

--- app/Http/Controllers/FetchUserInfoController.php
+++ app/Http/Controllers/FetchUserInfoController.php
@@ -16,23 +16,35 @@ class FetchUserInfoController extends Controller
     public function fetchBikeInfo()
     {
         $user_id = Session::get('user_id');
-        //$bikes = DB::table('bike_info')->where('id','=','1')->get();
-        $bikes = DB::select(DB::raw
-        ('select bike_infos.brand,bike_infos.bike_model,bike_infos.chasses_no,bike_infos.present_status
+
+        if (empty($user_id)) {
+            
+            echo '<script language="javascript">';
+            echo 'alert("Please Login First !!!")';
+            echo '</script>';
+            return view('user.search');
+
+
+        } else {
+
+            //$bikes = DB::table('bike_info')->where('id','=','1')->get();
+            $bikes = DB::select(DB::raw
+            ('select bike_infos.brand,bike_infos.bike_model,bike_infos.chasses_no,bike_infos.present_status
         FROM bike_infos
         JOIN bike_histories
         ON bike_infos.id=bike_histories.bike_id
         JOIN user_details
         ON bike_histories.user_id=user_details.id
-        WHERE user_details.id= :user_id'),array(
-        'user_id' => $user_id,)
-    );
+        WHERE user_details.id= :user_id'), array(
+                    'user_id' => $user_id,)
+            );
 
-        $personal_info = User_detail::where('id','=',$user_id)->get();
+            $personal_info = User_detail::where('id', '=', $user_id)->get();
 
-        //return $personal_info;
-        return view('user.userhome',compact('bikes','personal_info'));
-        //return view('user.userhome')->with('bikes',$bikes,'personal_info',$personal_info);
+            //return $personal_info;
+            return view('user.userhome', compact('bikes', 'personal_info'));
+            //return view('user.userhome')->with('bikes',$bikes,'personal_info',$personal_info);
+        }
     }
 
     public function addNewBike()
