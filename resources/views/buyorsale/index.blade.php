85cc320b9f75dad6f69ed0ddb02ed8afdd0b715b
shakilsaddam <saddam.shakil@gmail.com>
2018-02-06 16:37:42 +0600

    Router and Controller of BuyOrSale page.

---
+++ resources/views/buyorsale/index.blade.php
@@ -0,0 +1,81 @@
+<!DOCTYPE HTML>
+<html lang="zxx">
+
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <title>Search Cycle by Chasses no</title>
+    <link rel="icon" href="../del-code.com/images/icon/favicon.png" type="image/png">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="author" content="del-code.com" />
+
+    <link href='https://fonts.googleapis.com/css?family=Work+Sans:400,300,600,400italic,700' rel='stylesheet' type='text/css'>
+
+    <!-- Animate.css -->
+    <link rel="stylesheet" href="css/animate.css">
+    <!-- Icomoon Icon Fonts-->
+    <link rel="stylesheet" href="css/icomoon.css">
+    <!-- Bootstrap  -->
+    <link rel="stylesheet" href="css/bootstrap.css">
+
+    <!-- Magnific Popup -->
+    <link rel="stylesheet" href="css/magnific-popup.css">
+
+    <!-- Theme style  -->
+    <link rel="stylesheet" href="css/style.css">
+    <link rel="stylesheet" type="text/css" href="css/media-query.css">
+
+    <!-- Modernizr JS -->
+    <script src="js/modernizr-2.6.2.min.js"></script>
+    <!-- FOR IE9 below -->
+    <!--[if lt IE 9]>
+    <script src="js/respond.min.js"></script>
+    <![endif]-->
+
+
+
+</head>
+<body>
+
+<div class="col-xs-2">
+
+</div>
+
+<div id="fh5co-logo" class="col-xs-8">
+    <div class="col-sm-12">
+        <a href="index.html">Cycling Portal Bangladesh<span>.</span></a>
+        <br><br>
+    </div>
+    <div class="col-sm-12">
+        <div class="col-xs-12 text-center menu-1">
+            <ul>
+                <li class="active"><a href="/">Home</a></li>
+                <li><a href="blogs">Travelling Blog</a></li>
+                <li><a href="forum">Forum</a></li>
+                <li><a href="buysell">Buy or Sell</a></li>
+                <li><a href="about">About</a></li>
+
+            </ul>
+        </div>
+    </div>
+</div>
+
+
+<!-- jQuery -->
+<script src="js/jquery.min.js"></script>
+<!-- jQuery Easing -->
+<script src="js/jquery.easing.1.3.js"></script>
+<!-- Bootstrap -->
+<script src="js/bootstrap.min.js"></script>
+<!-- Waypoints -->
+<script src="js/jquery.waypoints.min.js"></script>
+<!-- Magnific Popup -->
+<script src="js/jquery.magnific-popup.min.js"></script>
+<script src="js/magnific-popup-options.js"></script>
+<!-- Main -->
+<script src="js/main.js"></script>
+
+
+</body>
+</html>
+
