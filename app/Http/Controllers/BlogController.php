10a7b1fa317346021a6b26dabdcee7af5e21c586
shakilsaddam <saddam.shakil@gmail.com>
2018-03-26 01:34:52 +0600

    blog template added.

--- app/Http/Controllers/BlogController.php
+++ app/Http/Controllers/BlogController.php
@@ -10,13 +10,18 @@ class BlogController extends Controller
     public function index()
     {
 
-        $blog_posts = DB::select(DB::raw('SELECT blog_posts.*,user_details.fname,user_details.lname,user_details.profile_photo FROM `blog_posts`, `user_details` WHERE blog_posts.posted_by=user_details.id'));
+        $blog_posts = DB::select(DB::raw('SELECT blog_posts.*,user_details.fname,user_details.lname,user_details.profile_photo FROM
+        `blog_posts`, `user_details` 
+        WHERE blog_posts.posted_by=user_details.id'));
         //return $blog_posts;
         return view('blog.index',compact('blog_posts'));
     }
 
     public function detail($blog_id)
     {
-        return $blog_id;
+        $blog_detail = DB::select(DB::raw
+        ('SELECT * FROM `blog_posts` WHERE id = :blog_id'), array("blog_id"=>$blog_id)
+        );
+        return view('blog.blog_detail');
     }
 }
