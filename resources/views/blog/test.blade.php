46b0c6b985c9ab08bba5575874792935188854fe
shakilsaddam <saddam.shakil@gmail.com>
2018-03-26 16:57:19 +0600

    blog homepage and detail page design completed.

--- resources/views/blog/test.blade.php
+++ resources/views/blog/test.blade.php
@@ -31,9 +31,9 @@
 <header class="header">
     <!-- Main Navbar-->
     <nav class="navbar navbar-expand-lg">
-        <div>
+       {{-- <div>
             <div class="search-area-inner d-flex align-items-center justify-content-center">
-                {{--<div class="close-btn"><i class="icon-close"></i></div>--}}
+                <div class="close-btn"><i class="icon-close"></i></div>
                 <div class="row d-flex justify-content-center">
                     <div class="col-md-8">
                         <form action="#">
@@ -45,7 +45,7 @@
                     </div>
                 </div>
             </div>
-        </div>
+        </div>--}}
         <div class="container">
             <!-- Navbar Brand -->
             <div class="navbar-header d-flex align-items-center justify-content-between">
@@ -71,6 +71,23 @@
         </div>
     </nav>
 </header>
+
+<nav class="navbar navbar-expand-lg">
+<div>
+    <div class="search-area-inner d-flex align-items-center justify-content-center">
+        <div class="row d-flex justify-content-center">
+            <div class="col-md-8">
+                <form action="#">
+                    <div class="form-group">
+                        <input type="search" name="search" id="search" placeholder="What are you looking for?">
+                        <button type="submit" class="submit"><i class="icon-search-1"></i></button>
+                    </div>
+                </form>
+            </div>
+        </div>
+    </div>
+</div>
+</nav>
 
 <!-- Page Footer-->
 <footer class="main-footer">
